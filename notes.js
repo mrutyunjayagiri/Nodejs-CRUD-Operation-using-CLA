b9acdaf38b7857d49c4475ebbc15647a5b0c48fd
mrutyunjaya.giri <mrutyunjaya.giri@gmail.com>
2020-03-01 00:30:49 +0530

    Data Changed

--- notes.js
+++ notes.js
@@ -63,8 +63,6 @@ const addNote = (title, body) => {
     const notes = loadNotes();
     const duplicateNote = notes.find((note) => note.title === title);
 
-debugger
-
     if (!duplicateNote) {
         notes.push({
             title: title,
